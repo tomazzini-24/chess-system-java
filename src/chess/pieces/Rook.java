14d0be9a4bde00ffec97d39d0bd1a1092135c71f
tomazzini-24 <tamiresoliveira.foxter@gmail.com>
2021-07-12 22:46:09 -0300

    Implementing possible moves of Rook

--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -8,7 +9,6 @@ public class Rook extends ChessPiece {
 
 	public Rook(Board board, Color color) {
 		super(board, color);
-		
 	}
 
 	@Override
@@ -18,8 +18,50 @@ public class Rook extends ChessPiece {
 	
 	@Override
 	public boolean[][] possibleMoves() {
-		boolean[][]mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
-		                             
-	    return mat;
+		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		
+		Position p = new Position(0, 0);
+		
+		// above
+		p.setValues(position.getRow() - 1, position.getColumn());
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {                              //Enquanto a posição estiver vaga, marcar como verdadeira
+			mat[p.getRow()][p.getColumn()] = true;
+			p.setRow(p.getRow() - 1);                                                                       //Alinha da posição vai avançar mais uma casa para cima -1
+		}
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		// left
+		p.setValues(position.getRow(), position.getColumn() - 1);
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {                             //Enquanto a posição estiver vaga, marcar como verdadeira
+			mat[p.getRow()][p.getColumn()] = true;
+			p.setColumn(p.getColumn() - 1);                                                                //Alinha da posição vai avançar mais uma casa para esquerda-1
+		}
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		// right
+		p.setValues(position.getRow(), position.getColumn() + 1);
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {                             //Enquanto a posição estiver vaga, marcar como verdadeira
+			mat[p.getRow()][p.getColumn()] = true;
+			p.setColumn(p.getColumn() + 1);                                                                //Alinha da posição vai avançar mais uma casa para esquerda +1
+		}
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		// below
+		p.setValues(position.getRow() + 1, position.getColumn());
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {                              //Enquanto a posição estiver vaga, marcar como verdadeira
+			mat[p.getRow()][p.getColumn()] = true;
+			p.setRow(p.getRow() + 1);                                                                       //Alinha da posição vai avançar mais uma casa para baixo +1
+		}
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		return mat;
 	}
 }
